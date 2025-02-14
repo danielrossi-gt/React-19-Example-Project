aa81b129245005eb4d93657eaffe256738b3a00d
= <=>
2025-02-14 09:44:25 -0300

    Ajustes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
 import Header from "./components/Header";
 import Entry from "./components/Entry";
+import data from "./data";
 
 export default function App() {
+  /*const entries = data.map((entry) => <Entry key={entry.id} entry={entry} />);*/
+  const entries = data.map((entry) => <Entry key={entry.id} {...entry} />);
+
   return (
     <>
       <Header />
       <main className="container">
-        {/* Passando as propriedades para o componente Entry */}
-        <Entry
-          img={{
-            src: "https://scrimba.com/links/travel-journal-japan-image-url",
-            alt: "Mount Fuji",
-          }}
-          title="Mount Fuji"
-          country="Japan"
-          googleMapsLink="https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9"
-          dates="12 Jan, 2021 - 24 Jan, 2021"
-          text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-        />
+        {/* Iterando sobre o array de objetos data e passando as propriedades para o componente Entry */}
+        {entries}
       </main>
     </>
   );
